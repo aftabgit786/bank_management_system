e6ae0484996a8b4752859ecd10d974e26a219328
aftabgit786 <gb485585@gmail.com>
2023-07-12 12:51:01 +0500

    formatting fix and remove extra tags

--- banksystem/accounts/views.py
+++ banksystem/accounts/views.py
@@ -6,4 +6,4 @@ def accounts(request, bank_name):
     user = request.user
     accounts_data = Account.objects.filter(user=user, bank__name=bank_name)
 
-    return render(request, 'accounts.html', {'accounts': accounts_data})
+    return render(request, 'accounts.html', {'account': accounts_data})
