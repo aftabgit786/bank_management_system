2bd2f648f1ad5f07e8a2451c56cea47f0093781d
aftabgit786 <gb485585@gmail.com>
2023-07-12 10:50:51 +0500

    direct import user and bank models

--- banksystem/accounts/models.py
+++ banksystem/accounts/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 from .choices import Types
+from users.models import User
+from banks.models import Bank
 
 
 class Account(models.Model):
@@ -9,8 +11,8 @@ class Account(models.Model):
     balance = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
 
-    user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='user_accounts')
-    bank = models.ForeignKey('banks.Bank', on_delete=models.CASCADE, related_name='bank_accounts')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_accounts')
+    bank = models.ForeignKey(Bank, on_delete=models.CASCADE, related_name='bank_accounts')
 
     def __str__(self):
         return self.number
