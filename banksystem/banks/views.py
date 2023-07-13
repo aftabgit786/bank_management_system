585b58a998b749ee58ae8ca6732c956503275fbf
aftabgit786 <gb485585@gmail.com>
2023-07-13 18:13:25 +0500

    fix formatting

--- banksystem/banks/views.py
+++ banksystem/banks/views.py
@@ -4,8 +4,7 @@ from .models import Bank
 
 def get_banks(request):
     user = request.user
-    total_bank_accounts = Bank.objects.filter(bank_accounts__user=user)
-    user_banks = Bank.objects.filter(bank_accounts__user=user).distinct()
-    total_account_counts = total_bank_accounts.count()
+    user_banks = Bank.objects.filter(bank_accounts__user=user)
+    accounts_counts = user_banks.count()
 
-    return render(request, 'banks.html', {'banks': user_banks, 'total_account': total_account_counts})
+    return render(request, 'banks.html', {'banks': user_banks.distinct(), 'total_accounts': accounts_counts})
