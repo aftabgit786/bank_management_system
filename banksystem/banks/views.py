dd3c20c296d9c6b858268abb789a36f62457914e
aftabgit786 <gb485585@gmail.com>
2023-07-14 16:02:17 +0500

    remove extra function

--- banksystem/banks/views.py
+++ banksystem/banks/views.py
@@ -5,6 +5,6 @@ from .models import Bank
 
 def get_banks(request):
     user = request.user
-    user_banks = Bank.objects.filter(bank_accounts__user=user).annotate(accounts_count=Count('bank_accounts')).distinct()
+    user_banks = Bank.objects.filter(bank_accounts__user=user).annotate(accounts_count=Count('bank_accounts'))
 
     return render(request, 'banks.html', {'banks': user_banks})
