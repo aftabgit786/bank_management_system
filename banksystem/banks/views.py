d02e830a3f6d8443abc0604592303681157f6e34
aftabgit786 <gb485585@gmail.com>
2023-07-14 14:13:21 +0500

    showing bank accounts count and also see query

--- banksystem/banks/views.py
+++ banksystem/banks/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
+from django.db.models import Count
 from .models import Bank
 
 
 def get_banks(request):
     user = request.user
-    user_banks = Bank.objects.filter(bank_accounts__user=user)
-    accounts_counts = user_banks.count()
+    user_banks = Bank.objects.filter(bank_accounts__user=user).annotate(accounts_count=Count('bank_accounts')).distinct()
 
-    return render(request, 'banks.html', {'banks': user_banks.distinct(), 'total_accounts': accounts_counts})
+    return render(request, 'banks.html', {'banks': user_banks})
