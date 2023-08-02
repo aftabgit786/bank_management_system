33f53e2343a3fb5d809cfc57f420d0acbab05e9d
aftabgit786 <gb485585@gmail.com>
2023-08-02 14:02:12 +0500

    change in urls file

--- banksystem/accounts/urls.py
+++ banksystem/accounts/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.GetBankAccountAllAPIView.as_view(), name='accounts'),
+    path('', views.GetBankAllAccountAPIView.as_view(), name='accounts'),
     path('<int:pk>/', views.GetBankAccountViaIdsAPIView.as_view(), name='accounts-details'),
 ]
