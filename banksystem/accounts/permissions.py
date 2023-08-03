c7a528ae2f2f6ccba82ad98f8326bf8c77cd3d43
aftabgit786 <gb485585@gmail.com>
2023-08-03 13:54:10 +0500

    remove extra code

--- banksystem/accounts/permissions.py
+++ banksystem/accounts/permissions.py
@@ -5,7 +5,4 @@ class MyCustomPermission(BasePermission):
     message = "Only Aftab can enter"
 
     def has_permission(self, request, view):
-        if not request.user.is_authenticated:
-            return False
-
         return request.user.first_name == 'aftab'
