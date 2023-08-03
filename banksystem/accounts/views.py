a7dffea00175cc83c22a70a4374eeca158843c90
aftabgit786 <gb485585@gmail.com>
2023-08-03 18:09:10 +0500

    remove defualt value

--- banksystem/accounts/views.py
+++ banksystem/accounts/views.py
@@ -29,7 +29,7 @@ class AdminBankListAPIView(generics.ListAPIView):
 
     def get_queryset(self):
         queryset = Account.objects.all()
-        user_id = self.request.query_params.get('user', None)
-        if user_id is not None:
+        user_id = self.request.query_params.get('user')
+        if user_id:
             queryset = queryset.filter(user_id=user_id)
         return queryset
